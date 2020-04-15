8f7ab99b9a8f9e8401fa7dc592eeba7f48569986
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-15 21:53:33 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,7 +1,11 @@
 def using_times
 	7.times do
-	  puts "Windgardium Leviosa"
+	  puts "Wingardium Leviosa"
 	end
 end
 
 using_times
+
+expected block to output "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Levio
+sa\n" to stdout, but output "Windgardium Leviosa\nWindgardium Leviosa\nWindgardium Leviosa\nWindgardium Leviosa\nWindgardium Leviosa\nWindgardium Leviosa\nWindgardium Le
+viosa\n"
