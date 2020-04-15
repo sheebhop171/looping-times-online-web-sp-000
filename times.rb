56f991b2f208712e3a4d5b94c7d1df1c1b54d530
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-15 21:53:39 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -5,7 +5,3 @@ def using_times
 end
 
 using_times
-
-expected block to output "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Levio
-sa\n" to stdout, but output "Windgardium Leviosa\nWindgardium Leviosa\nWindgardium Leviosa\nWindgardium Leviosa\nWindgardium Leviosa\nWindgardium Leviosa\nWindgardium Le
-viosa\n"
